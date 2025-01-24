c9a0996cac7212dc9d0fc746002c239334d82bcb
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-24 18:51:12 +0200

    jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})], //no referesh token is going be used
   controllers: [AuthController],
   providers: [AuthService],
 })
