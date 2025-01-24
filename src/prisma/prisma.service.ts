36132eb35bab56c949019556b4ceadc0cb7ccefa
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-24 18:22:12 +0200

    save

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:123@localhost:5434/nest',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
+    console.log(
+      "🔥 :: config.get('DATABASE_URL') ::",
+      config.get('DATABASE_URL'),
+    );
   }
 }
