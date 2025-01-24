52cc81cbae40e6dec1b46f4e8598b8d3d1619ef1
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-24 11:08:42 +0200

    save

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,7 +7,8 @@ export class AuthController {
   constructor(private AuthService: AuthService) {}
 
   @Post('signup')
-  singup() {
+  singup(@Req() req: Request) {
+    console.log('🔥 :: req ::', req.body);
     return this.AuthService.signup();
   }
 
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signup() {
     console.log('🔥 :: singup ::');
-    return 'You are signed up';
+    return 'You are signed up!';
   }
   signin() {
     console.log('🔥 :: singup ::');
