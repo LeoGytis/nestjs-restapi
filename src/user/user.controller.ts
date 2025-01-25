0e8430cdbbeb3982b86dfd6aa44965177d0300c6
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-25 13:23:06 +0200

    save

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+// @Controller('users')
+// export class UserController {
+//   @UseGuards(JwtGuard)
+//   @Get('me')
+//   getMe(@Req() req: Request) {
+//     console.log('🔥 :: Req ::', req.user);
+//     return req.user;
+//   }
+// }
+
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  @UseGuards(AuthGuard('jwt'))
+  // constructor(private userService: UserService) {}
   @Get('me')
-  getMe(@Req() req: Request) {
-    console.log('🔥 :: Req ::', req.url);
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
+
+  // @Patch()
+  // editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+  //   return this.userService.editUser(userId, dto);
+  // }
 }
