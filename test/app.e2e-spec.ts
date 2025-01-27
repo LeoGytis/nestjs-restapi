6ddbc06f5690a43d5882582dda602682ab55659c
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-27 12:26:19 +0200

    save

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,11 +1,13 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 // Check auto imports has to begni with '../'
 
 describe('App e2e', () => {
   let app: INestApplication;
+  let prisma: PrismaService;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -15,6 +17,9 @@ describe('App e2e', () => {
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
 
   afterAll(async () => {
