0cc1dc769ded4570a9d4eac268f565e39977e4a4
Gytis Leonavicius <101097792+LeoGytis@users.noreply.github.com>
2025-01-27 13:03:13 +0200

    saved

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { AuthDto } from 'src/auth/dto';
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
-
 // Check auto imports has to begni with '../'
 
 describe('App e2e', () => {
@@ -17,8 +18,10 @@ describe('App e2e', () => {
 
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
+    pactum.request.setBaseUrl('http://localhost:3333');
     await prisma.cleanDb();
   });
 
@@ -26,4 +29,31 @@ describe('App e2e', () => {
     await app.close();
   });
   it.todo('should pass test');
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'gytisn@gmail.com',
+      password: '123',
+    };
+
+    describe('Sign UP', () => {
+      it('should signup', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+    describe('Sign IN', () => {
+      // it('should signin', () => {
+      //   return pactum
+      //     .spec()
+      //     .post('/auth/signin')
+      //     .withBody(dto)
+      //     .expectStatus(200)
+      //     .stores('userAt', 'access_token');
+      // });
+    });
+  });
 });
